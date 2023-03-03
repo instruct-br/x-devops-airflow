8e2b9a05de286413f29f5f6979c54794d9e26457
Renan Dantas <renan.dantas@instruct.com.br>
2023-03-03 16:54:27 -0300

    feat: adds powerpoint apresentation

--- dags/sql/pokemon_schema.sql
+++
@@ -1,8 +0,0 @@
--- create pet table
-CREATE TABLE IF NOT EXISTS pokemon (
-    create_on DATE not null default CURRENT_DATE,
-    update_on DATE not null default CURRENT_DATE,
-    pokemon_id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    weight INTEGER NOT NULL,
-    abilities VARCHAR[]);
